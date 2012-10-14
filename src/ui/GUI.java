92e8a5df769ad4efc70014233e5cc97d0e2fe9c5
a0088278 <a0088278@A0088278.stu.nus.edu.sg>
2012-10-14 13:58:53 +0800

    renamed inputText to GUI. Connected GUI to CommandProcessor.

--- src/ui/GUI.java
+++ src/ui/GUI.java
@@ -27,113 +27,119 @@
  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */ 
+ */
 package ui;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.io.IOException;
+
 import javax.swing.*;
 
-public class inputText extends JPanel implements ActionListener {
+import commandLogic.CommandProcessor;
+
+public class GUI extends JPanel implements ActionListener {
+	private static final String ERROR_COULD_NOT_ACCESS_STORAGE = "Error: Could not access storage.";
+	private static final String WELCOME_MESSAGE = "Welcome to Jimi, your friendly neighbourhood task manager.";
+	private static final long serialVersionUID = 1L;
 	protected JTextArea textArea;
-	protected JTextField textField= new JTextField(30);;
+	protected JTextField textField = new JTextField(30);
+	private CommandProcessor commandProcessor;
+	private final static String NEW_LINE = "\n";
 
-	private final static String newline = "\n";
-
-	public inputText() {
-
+	public GUI() {
 		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 
-		//Create and set up the layered pane.
+		// Create and set up the layered pane.
 		textField.addActionListener(this);
 		JPanel layeredPane1 = new JPanel();
-		layeredPane1.setBorder(BorderFactory.createTitledBorder(
-				"input"));
+		layeredPane1.setBorder(BorderFactory.createTitledBorder("input"));
 		layeredPane1.add(textField);
 		layeredPane1.setOpaque(true);
-		
+
 		JPanel layeredPane = new JPanel();
 		JLabel bgLabel = new JLabel();
-		 ImageIcon icon = createImageIcon("images/bg.gif");
+		ImageIcon icon = createImageIcon("images/bg.gif");
 
-		 //Create and add the Duke label to the layered pane.
-		 bgLabel = new JLabel(icon);
-	        if (icon == null) {
-	            System.err.println("Picture not found; using white rectangle instead.");
-	            bgLabel.setOpaque(true);
-	            bgLabel.setBackground(Color.cyan);
-	        }
-	        layeredPane.add(bgLabel);
-	       // layeredPane.moveToBack(bgLabel);
-		 
-		 
-		//Add control pane and layered pane to this JPanel.
+		// Create and add the Duke label to the layered pane.
+		bgLabel = new JLabel(icon);
+		if (icon == null) {
+			System.err.println("Picture not found; using white rectangle instead.");
+			bgLabel.setOpaque(true);
+			bgLabel.setBackground(Color.cyan);
+		}
+		layeredPane.add(bgLabel);
+		// layeredPane.moveToBack(bgLabel);
+
+		// Add control pane and layered pane to this JPanel.
 		add(Box.createRigidArea(new Dimension(0, 10)));
 		add(createControlPanel());
 		add(layeredPane1);
 		add(layeredPane);
+		try{
+			commandProcessor = new CommandProcessor();
+		}catch(IOException e){
+			textArea.append(NEW_LINE + ERROR_COULD_NOT_ACCESS_STORAGE);
+		}
 		
 	}
-	
-	
 
 	private Component createControlPanel() {
 		textArea = new JTextArea(7, 35);
 		textArea.setEditable(false);
+		textArea.setText(WELCOME_MESSAGE);
 		JScrollPane scrollPane = new JScrollPane(textArea);
 
 		JPanel controls = new JPanel();
 		controls.add(scrollPane);
 
-		controls.setBorder(BorderFactory.createTitledBorder(
-				"output"));
+		controls.setBorder(BorderFactory.createTitledBorder("output"));
 		return controls;
 	}
 
-    /** Returns an ImageIcon, or null if the path was invalid. */
+	/** Returns an ImageIcon, or null if the path was invalid. */
 	protected static ImageIcon createImageIcon(String path) {
-        java.net.URL imgURL = inputText.class.getResource(path);
-        if (imgURL != null) {
-            return new ImageIcon(imgURL);
-        } else {
-            System.err.println("Couldn't find file: " + path);
-            return null;
-        }
-    }
-    
+		java.net.URL imgURL = GUI.class.getResource(path);
+		if (imgURL != null) {
+			return new ImageIcon(imgURL);
+		} else {
+			System.err.println("Couldn't find file: " + path);
+			return null;
+		}
+	}
+
 	public void actionPerformed(ActionEvent evt) {
-		String text = textField.getText();
-		textArea.append(text + newline);
+		String command = textField.getText();
+		String output = commandProcessor.processCommand(command);
+		textArea.append(NEW_LINE + output);
 		textField.selectAll();
 
-		//Make sure the new text is visible, even if there
-		//was a selection in the text area.
+		// Make sure the new text is visible, even if there
+		// was a selection in the text area.
 		textArea.setCaretPosition(textArea.getDocument().getLength());
 	}
 
 	/**
-	 * Create the GUI and show it.  For thread safety,
-	 * this method should be invoked from the
-	 * event dispatch thread.
+	 * Create the GUI and show it. For thread safety, this method should be
+	 * invoked from the event dispatch thread.
 	 */
 	private static void createAndShowGUI() {
 
-		//Create and set up the window.
+		// Create and set up the window.
 		JFrame frame = new JFrame("Jimi");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		//Add contents to the window.
-		frame.add(new inputText());
+		// Add contents to the window.
+		frame.getContentPane().add(new GUI());
 
-
-		//Display the window.
+		// Display the window.
 		frame.pack();
 		frame.setVisible(true);
 	}
 
 	public static void main(String[] args) {
-		//Schedule a job for the event dispatch thread:
-		//creating and showing this application's GUI.
+		// Schedule a job for the event dispatch thread:
+		// creating and showing this application's GUI.
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				createAndShowGUI();
