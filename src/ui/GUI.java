1b28175efd8145e4ba50e3fcf44c568f0ee4f2ba
a0088278 <a0088278@A0088278.stu.nus.edu.sg>
2012-10-24 09:34:29 +0800

    Fixed GUI. Should be able to display tasks now

--- src/ui/GUI.java
+++ src/ui/GUI.java
@@ -47,63 +47,57 @@ public class GUI extends JPanel implements ActionListener {
 	JTextField textField = new JTextField(32);
 	CommandProcessor commandProcessor;
 	JLabel commandOutputLabel = null;
-	
+
 	public GUI() {
 		ImageIcon icon = createImageIcon(BACKGROUND_IMG);
 		JLabel bgLabel = new JLabel(icon);
 		bgLabel.setSize(bgLabel.getPreferredSize());
 
-        add( addTraversalKeys() );
+		add(addTraversalKeys());
 
 		// Create and set up the layered pane.
 		JPanel forgroundPanel = new JPanel(new GridBagLayout());
 		forgroundPanel.setOpaque(false);
 		forgroundPanel.setPreferredSize(new Dimension(400, 50));
-		
 
 		JLayeredPane layeredPane = new JLayeredPane();
 		layeredPane.setPreferredSize(bgLabel.getPreferredSize());
-		//layeredPane.add(bgLabel, JLayeredPane.DEFAULT_LAYER);
+		// layeredPane.add(bgLabel, JLayeredPane.DEFAULT_LAYER);
 		layeredPane.add(forgroundPanel, JLayeredPane.PALETTE_LAYER);
 
-		textField.setBackground(new java.awt.Color(220,219,219));
+		textField.setBackground(new java.awt.Color(220, 219, 219));
 		textField.setBorder(empty);
 		textField.addActionListener(this);
 
 		forgroundPanel.add(textField);
-		
 
 		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 
-		
 		add(createControlPanel());
 		add(Box.createRigidArea(new Dimension(0, 10)));
 		add(forgroundPanel);
 
-		/*
-		JLabel commandOutputLabel = new JLabel("");
+		commandOutputLabel = new JLabel("");
 		commandOutputLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 		add(commandOutputLabel);
 		add(layeredPane);
 
-		try{
-			CommandProcessor commandProcessor = new CommandProcessor();
+		try {
+			commandProcessor = new CommandProcessor();
 			textArea.append(commandProcessor.getCurrentListOfTasks());
-		}catch(IOException e){
+		} catch (IOException e) {
 			textArea.append(NEW_LINE + ERROR_COULD_NOT_ACCESS_STORAGE);
 		}
-*/
+
 	}
 
-	private Component createControlPanel(){
-		
+	private Component createControlPanel() {
+
 		textArea.setEditable(false);
 		textArea.setText("");
 		textArea.setLineWrap(true);
 		textArea.setWrapStyleWord(true);
 		textArea.setText(WELCOME_MESSAGE);
-
-		GUIdata();
 
 		JScrollPane scrollPane = new JScrollPane(textArea);
 		scrollPane.setPreferredSize(new Dimension(400, 200));
@@ -117,7 +111,6 @@ public class GUI extends JPanel implements ActionListener {
 		verticalBox.add(scrollPane);
 		verticalBox.setPreferredSize(new Dimension(400, 200));
 
-
 		JPanel controls = new JPanel();
 		controls.add(verticalBox);
 		controls.setBorder(BorderFactory.createTitledBorder(BORDER_TITLE));
@@ -125,106 +118,69 @@ public class GUI extends JPanel implements ActionListener {
 		return controls;
 	}
 
-	 //  Add Tab and Shift-Tab KeyStrokes back as focus traversal keys.
-    //  Seems more complicated then just using null, but at least
-    //  it shows how to add a KeyStroke as a focus traversal key.
+	// Add Tab and Shift-Tab KeyStrokes back as focus traversal keys.
+	// Seems more complicated then just using null, but at least
+	// it shows how to add a KeyStroke as a focus traversal key.
 
-    private JComponent addTraversalKeys()
-    {
-        JScrollPane scrollPane = new JScrollPane( textArea );
-        scrollPane.getVerticalScrollBar().setFocusable(false);
+	private JComponent addTraversalKeys() {
+		JScrollPane scrollPane = new JScrollPane(textArea);
+		scrollPane.getVerticalScrollBar().setFocusable(false);
 
-        Set set = new HashSet( textArea.getFocusTraversalKeys(
-            KeyboardFocusManager.FORWARD_TRAVERSAL_KEYS ) );
-        set.add( KeyStroke.getKeyStroke( "TAB" ) );
-        textArea.setFocusTraversalKeys(
-            KeyboardFocusManager.FORWARD_TRAVERSAL_KEYS, set );
+		Set set = new HashSet(
+				textArea.getFocusTraversalKeys(KeyboardFocusManager.FORWARD_TRAVERSAL_KEYS));
+		set.add(KeyStroke.getKeyStroke("TAB"));
+		textArea.setFocusTraversalKeys(
+				KeyboardFocusManager.FORWARD_TRAVERSAL_KEYS, set);
 
-        set = new HashSet( textArea.getFocusTraversalKeys(
-            KeyboardFocusManager.BACKWARD_TRAVERSAL_KEYS ) );
-        set.add( KeyStroke.getKeyStroke( "shift TAB" ) );
-        textArea.setFocusTraversalKeys(
-            KeyboardFocusManager.BACKWARD_TRAVERSAL_KEYS, set );
+		set = new HashSet(
+				textArea.getFocusTraversalKeys(KeyboardFocusManager.BACKWARD_TRAVERSAL_KEYS));
+		set.add(KeyStroke.getKeyStroke("shift TAB"));
+		textArea.setFocusTraversalKeys(
+				KeyboardFocusManager.BACKWARD_TRAVERSAL_KEYS, set);
 
-        return scrollPane;
-    }
-
-    class TabAction extends AbstractAction
-    {
-        private boolean forward;
-
-        public TabAction(boolean forward)
-        {
-            this.forward = forward;
-        }
-
-        public void actionPerformed(ActionEvent e)
-        {
-            if (forward)
-                tabForward();
-            else
-                tabBackward();
-        }
-
-        private void tabForward()
-        {
-            final KeyboardFocusManager manager =
-                KeyboardFocusManager.getCurrentKeyboardFocusManager();
-            manager.focusNextComponent();
-
-            SwingUtilities.invokeLater(new Runnable()
-            {
-                public void run()
-                {
-                    if (manager.getFocusOwner() instanceof JScrollBar)
-                        manager.focusNextComponent();
-                }
-            });
-        }
-
-        private void tabBackward()
-        {
-            final KeyboardFocusManager manager =
-                KeyboardFocusManager.getCurrentKeyboardFocusManager();
-            manager.focusPreviousComponent();
-
-            SwingUtilities.invokeLater(new Runnable()
-            {
-                public void run()
-                {
-                    if (manager.getFocusOwner() instanceof JScrollBar)
-                        manager.focusPreviousComponent();
-                }
-            });
-        }
-    }
-
-	
-	private void GUIdata(){
-		//parse file into a single string
-
-		Scanner s = null;
-
-		try {
-			s = new Scanner(new BufferedReader(new FileReader("taskrecords.txt")));
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		String output = null;
-		while (s.hasNext()) {
-			s.useDelimiter(",\\s*");
-			output = output + s.nextLine() + "\n";
-		}
-
-
-		s.close();
-		textArea.append(output);
+		return scrollPane;
 	}
 
+	class TabAction extends AbstractAction {
+		private boolean forward;
 
+		public TabAction(boolean forward) {
+			this.forward = forward;
+		}
 
+		public void actionPerformed(ActionEvent e) {
+			if (forward)
+				tabForward();
+			else
+				tabBackward();
+		}
 
+		private void tabForward() {
+			final KeyboardFocusManager manager = KeyboardFocusManager
+					.getCurrentKeyboardFocusManager();
+			manager.focusNextComponent();
+
+			SwingUtilities.invokeLater(new Runnable() {
+				public void run() {
+					if (manager.getFocusOwner() instanceof JScrollBar)
+						manager.focusNextComponent();
+				}
+			});
+		}
+
+		private void tabBackward() {
+			final KeyboardFocusManager manager = KeyboardFocusManager
+					.getCurrentKeyboardFocusManager();
+			manager.focusPreviousComponent();
+
+			SwingUtilities.invokeLater(new Runnable() {
+				public void run() {
+					if (manager.getFocusOwner() instanceof JScrollBar)
+						manager.focusPreviousComponent();
+				}
+			});
+		}
+	}
 
 	/** Returns an ImageIcon, or null if the path was invalid. */
 	protected static ImageIcon createImageIcon(String path) {
@@ -239,9 +195,9 @@ public class GUI extends JPanel implements ActionListener {
 
 	public void actionPerformed(ActionEvent evt) {
 		String command = textField.getText();
-		
+
 		String output = commandProcessor.processCommand(command);
-		
+
 		commandOutputLabel.setText(output);
 		textArea.setText(commandProcessor.getCurrentListOfTasks());
 		textField.selectAll();
@@ -256,14 +212,15 @@ public class GUI extends JPanel implements ActionListener {
 	/**
 	 * Create the GUI and show it. For thread safety, this method should be
 	 * invoked from the event dispatch thread.
-	 * @throws FileNotFoundException 
+	 * 
+	 * @throws FileNotFoundException
 	 */
 	private static void createAndShowGUI() throws FileNotFoundException {
 		// Create and set up the window.
 		JFrame frame = new JFrame(FRAME_NAME);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		//Create and set up the content pane.
+		// Create and set up the content pane.
 		frame.getContentPane().add(new GUI());
 
 		// Display the window.
