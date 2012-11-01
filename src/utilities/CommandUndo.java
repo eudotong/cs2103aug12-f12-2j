9d120ad7a742b98de4afd9d49319a84ff2fe6b85
Amanda <Amanda@Amanda-PC.nus.edu.sg>
2012-11-01 18:57:20 +0800

    merge and updated logging to file

---
+++ src/utilities/CommandUndo.java
@@ -0,0 +1,28 @@
+package utilities;
+
+import storage.TaskRecords;
+
+public class CommandUndo implements Command {
+	private static final boolean IS_REVERSIBLE = false;
+
+	@Override
+	public Command reverseCommand() {
+		return null;
+	}
+
+	@Override
+	public boolean isReversible() {
+		return IS_REVERSIBLE;
+	}
+
+	@Override
+	public CommandType getCommandType() {
+		return CommandType.UNDO;
+	}
+
+	@Override
+	public String processCommand(TaskRecords taskRecords) {
+		return "";
+	}
+
+}
