b6626f45eb04ac811e28b50c7394f476262cbfd3
a0088278 <a0088278@A0088278.stu.nus.edu.sg>
2012-11-09 03:48:24 +0800

    Debugged CommandParser and added Junit test for CommandParser.

--- src/utilities/CommandAdd.java
+++ src/utilities/CommandAdd.java
@@ -11,6 +11,7 @@ import storage.TaskRecords;
 public class CommandAdd implements Command {
 	private static final String MESSAGE_ERROR_CANNOT_ADD = "Task could not be added.";
 	private static final String MESSAGE_SUCCESS = "%s was added";
+	
 
 	private boolean isReversible = true;
 	private Task taskToBeAdded;
