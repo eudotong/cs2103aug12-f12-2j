801205cbdd450a9c626ecf5690557a293293fa64
a0088278 <a0088278@A0088278.stu.nus.edu.sg>
2012-11-10 13:17:49 +0800

    Changed commands to another kind of message

--- src/utilities/CommandMark.java
+++ src/utilities/CommandMark.java
@@ -9,7 +9,7 @@ import storage.TaskRecords;
  */
 public class CommandMark implements Command {
 	private static final String MESSAGE_ERROR_CANNOT_DELETE = "Task could not be deleted.";
-	private static final String MESSAGE_SUCCESS = "%s was deleted";
+	private static final String MESSAGE_SUCCESS = "Task deleted: %s";
 
 	private boolean isReversible = true;
 	private Task taskToBeDeleted;
