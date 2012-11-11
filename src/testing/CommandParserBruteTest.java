8144d06704354029144f5c95390405d909d439d9
Ernest <Ernest@Freedom00.esr6650>
2012-11-11 23:15:53 +0800

    Refactored CommandParserBruteTest

---
+++ src/testing/CommandParserBruteTest.java
@@ -0,0 +1,108 @@
+/**
+ * Change test parameters to do targeted brute testing with random string
+ * 
+ * @author A0081241U
+ */
+
+package testing;
+
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import commandLogic.CommandParser;
+import exceptions.CommandCouldNotBeParsedException;
+import exceptions.IncorrectDateFormatException;
+import exceptions.StartTimeAfterEndTimeException;
+
+import testing.stringGenerator.TextGenerator;
+import utilities.Command;
+import utilities.Task;
+
+public class CommandParserBruteTest {
+	
+	private static DateTimeFormatter DATE_FORMATTER = DateTimeFormat.forPattern("dd/MM/yyyy HH:mm");
+	private static CommandParser COMMAND_PARSER = new CommandParser();
+	private static final String WHITE_SPACE = " ";
+	private static int successCounter = 0;
+	private static int testCounter = 0;
+	
+	private static String randomString;
+	private static Command actualCommand;
+	private static Task expectedTask;
+
+
+	//Test parameters
+	private final static int[] TEST_COMMAND_LENGTH = {30, 50, 70};
+	private final static int TEST_ITERATIONS = 50;
+
+	private final static String TEST_COMMAND_TYPE = "add";
+	
+	// Expected task
+	private final static DateTime TEST_START_DATE_TIME = DATE_FORMATTER.parseDateTime("24/11/2012 10:00");	
+	private final static DateTime TEST_END_DATE_TIME = null;
+	
+	// Actual command
+	private final static String TEST_DATE_TIME_STRING = "24th Nov 10am";	
+	private final static String TEST_COMMAND = TEST_COMMAND_TYPE + WHITE_SPACE + TEST_DATE_TIME_STRING + WHITE_SPACE + "%s";
+	
+	public static void main(String args[]){
+		
+		for (int length : TEST_COMMAND_LENGTH){
+			
+			TextGenerator textGenerator = new TextGenerator(length);
+		
+			for (int i = 0; i < TEST_ITERATIONS; i++){
+				
+				System.out.println("Test length = " + length + "\t Test iteration " + i + " of " + TEST_ITERATIONS);
+				
+				try {
+					
+					randomString = textGenerator.next();
+					expectedTask = new Task(randomString, TEST_START_DATE_TIME, TEST_END_DATE_TIME);
+					actualCommand = COMMAND_PARSER.parseCommand(String.format(TEST_COMMAND, randomString));
+			
+				} catch (CommandCouldNotBeParsedException e) {
+					e.printStackTrace();
+				} catch (StartTimeAfterEndTimeException e) {
+					e.printStackTrace();
+				} catch (IncorrectDateFormatException e) {
+					e.printStackTrace();
+				}
+				
+				
+				if (isSuccessful()){
+					System.out.println("\"" + randomString + "\" is successful" );
+					
+					successCounter++;
+				}
+				
+				testCounter++;
+				
+			}
+
+		}
+		
+		System.out.println("Success rate = " + successCounter + "/" + testCounter);
+	}
+	
+	private static boolean isSuccessful(){
+		
+		boolean result;
+		
+		String expected = expectedTask.toString();
+		String actual = actualCommand.toString();
+		
+		
+		if (expected.compareTo(actual) ==0 ){
+			result = true;
+		}
+		else {			
+			System.out.println("Expected: " + expected);
+			System.out.println("Actual:   " + actual);
+			result = false;
+		}
+		
+		return result;
+	}
+}
